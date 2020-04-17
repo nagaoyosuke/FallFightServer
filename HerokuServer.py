ce9f26a99dfaaddd0831b877aa0c457047f8ff25
nagaoyosuke <hw17a124@oecu.jp>
2020-04-17 10:10:16 +0900

    no message

--- HerokuServer.py
+++ HerokuServer.py
@@ -8,14 +8,12 @@ import socket
 import threading
 
 from bottle import route, run
-from LobbyBase import LobbyBase
-
 # from LobbyBase import LobbyBase
 
-bind_ip = "https://fallfightserver.herokuapp.com" #お使いのサーバーのホスト名を入れます
-bind_port = int(os.getenv("PORT", 5000)) #クライアントで設定したPORTと同じもの指定してあげます
+bind_ip = "https://fallfightserver.herokuapp.com" 
+bind_port = int(os.getenv("PORT", 5000)) 
 
-Lobby = LobbyBase(16)
+# Lobby = LobbyBase(16)
 # thread processing a task from clients
 def on_message(client_socket,addr):
     while True:
@@ -24,23 +22,23 @@ def on_message(client_socket,addr):
             # print data (max buffer size 1024) sent from client
             print ("Received: %s" % request)
             # send a message "Ack" to client
-            # client_socket.send("Ack!, connecting..".encode('utf_8'))
+            client_socket.send(request.encode('utf_8'))
 
-            data = json.loads(request)
+            # data = json.loads(request)
 
-            print(data['state'])
-            print(data)
+            # print(data['state'])
+            # print(data)
 
-            if data['state'] == 'Init':
-                sendData = InitMessage(data)
-            elif data['state'] == 'Login':
-                Lobby.Login(client_socket,addr[0],addr[1],data)
-            elif data['state'] == 'MemberList':
-                Lobby.LobbyMemberList(client_socket,data)
-            elif data['state'] == 'OpenChat':
-                Lobby.OpenChat(client_socket,data)
-            elif data['state'] == 'DirectChat':
-                Lobby.DirectChat(client_socket,data)    
+            # if data['state'] == 'Init':
+            #     sendData = InitMessage(data)
+            # elif data['state'] == 'Login':
+            #     Lobby.Login(client_socket,addr[0],addr[1],data)
+            # elif data['state'] == 'MemberList':
+            #     Lobby.LobbyMemberList(client_socket,data)
+            # elif data['state'] == 'OpenChat':
+            #     Lobby.OpenChat(client_socket,data)
+            # elif data['state'] == 'DirectChat':
+            #     Lobby.DirectChat(client_socket,data)    
         except socket.error:
             print("Lost")
             client_socket.close()
@@ -59,12 +57,9 @@ def hello():
 
 # Main
 if __name__ == "__main__":
-    # create socket object
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # bind ip + port as a server 
     server.bind((socket.gethostname(), bind_port))
-    # listen with maximum 5 waiting queues
-    server.listen(5)  ### server while loop
+    server.listen(5)  
     print ("Listening on %s: %d" % (bind_ip, bind_port))
     while True:
         # event: a conncetion from client

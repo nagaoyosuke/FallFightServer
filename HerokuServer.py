49477e90449c505fd99a907ba7bdf2dddb031651
nagaoyosuke <hw17a124@oecu.jp>
2020-04-17 10:34:36 +0900

    no message

--- HerokuServer.py
+++ HerokuServer.py
@@ -18,6 +18,7 @@ def on_message(client_socket,addr):
             request = client_socket.recv(1024)
             # print data (max buffer size 1024) sent from client
             print ("Received: %s" % request)
+            client_socket.close()
 
             # send a message "Ack" to client
             # client_socket.send(request)

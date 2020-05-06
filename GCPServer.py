c9a13c72364200b325ff9a941027697a1a666ece
nagaoyosuke <hw17a124@oecu.jp>
2020-05-07 03:10:46 +0900

    GCPでの動作確認

--- GCPServer.py
+++ GCPServer.py
@@ -1,5 +1,5 @@
 import logging
-import psycopg2
+# import psycopg2
 import os
 import json
 import random
@@ -8,14 +8,18 @@ import socket
 import threading
 
 from bottle import route, run
-# from LobbyBase import LobbyBase
 
-# Lobby = LobbyBase(16)
+
+from LobbyBase import LobbyBase
+
+Lobby = LobbyBase(16)
 
 def CreateServer():
     # bind_ip = socket.gethostname() #お使いのサーバーのホスト名を入れます
+    # bind_ip = "34.82.63.106" #お使いのサーバーのホスト名を入れます
     bind_ip = "0.0.0.0" #お使いのサーバーのホスト名を入れます
     bind_port = int(os.getenv("PORT", 5000)) 
+    # bind_port = 8081 
 
     with socket.create_server((bind_ip,bind_port), family=socket.AF_INET, dualstack_ipv6=False) as server:
         # create socket object
@@ -41,6 +45,8 @@ def on_message(client_socket,addr):
             request = client_socket.recv(1024)
              # 切断された
             if len(request) == 0:
+                Lobby.Logout(client_socket)
+                client_socket.close()
                 break
             # print data (max buffer size 1024) sent from client
             print ("Received: %s" % request)
@@ -55,16 +61,16 @@ def on_message(client_socket,addr):
 
             client_socket.send("OK".encode('utf_8'))
 
-            # if data['state'] == 'Init':
-            #     sendData = InitMessage(data)
-            # elif data['state'] == 'Login':
-            #     Lobby.Login(client_socket,addr[0],addr[1],data)
-            # elif data['state'] == 'MemberList':
-            #     Lobby.LobbyMemberList(client_socket,data)
-            # elif data['state'] == 'OpenChat':
-            #     Lobby.OpenChat(client_socket,data)
-            # elif data['state'] == 'DirectChat':
-            #     Lobby.DirectChat(client_socket,data)    
+            if data['state'] == 'Init':
+                sendData = InitMessage(data)
+            elif data['state'] == 'Login':
+                Lobby.Login(client_socket,addr[0],addr[1],data)
+            elif data['state'] == 'MemberList':
+                Lobby.LobbyMemberList(client_socket,data)
+            elif data['state'] == 'OpenChat':
+                Lobby.OpenChat(client_socket,data)
+            elif data['state'] == 'DirectChat':
+                Lobby.DirectChat(client_socket,data)    
         except socket.error:
             print("Lost")
             break
@@ -79,6 +85,11 @@ def on_message(client_socket,addr):
             continue
     # client_socket.close()
 
+# @route('/')
+# def hello():
+#     print("hello")
+#     return ""
+
 @route('/')
 def hello():
     print("hello")
@@ -86,4 +97,8 @@ def hello():
 
 # Main
 if __name__ == "__main__":
-   CreateServer()
+    # t = threading.Thread(target=CreateServer)
+    # t.daemon = True
+    # t.start()
+    # app.run(debug=True)
+    CreateServer()
